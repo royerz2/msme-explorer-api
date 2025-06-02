1e4868a2d52136546a7a16af66eb32058af5e49d
royerz2 <royerz.2002@gmail.com>
2025-06-02 15:12:29 +0200

    adapt to port 5001

---
+++ gunicorn_config.py
@@ -0,0 +1,24 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:5001"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# Process naming
+proc_name = 'msme_analysis_api'
+
+# SSL (uncomment and configure if using HTTPS)
+# keyfile = 'path/to/keyfile'
+# certfile = 'path/to/certfile' 
